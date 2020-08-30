0e3238d11f271e4f12898d2acd89a2e8062fce21
ffarall <37149322+ffarall@users.noreply.github.com>
2020-08-30 14:00:47 -0300

    Changing name to BlinkTemplate

--- BlinkTemplate/Debug/objects.mk
+++ BlinkTemplate/Debug/objects.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-USER_OBJS := C:/nxp/Blink/source/gpio.o
+USER_OBJS := C:/Users/facun/OneDrive/Desktop/ITBA/7C_Laboratorio_de_Microprocesadores/Usefuls/BlinkTemplate/source/gpio.o
 
 LIBS :=
 
