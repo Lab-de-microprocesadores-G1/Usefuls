89cee489e7e4208bd5608fa3290be7dc8d2a6be4
gonzadavidov <gdavidov@itba.edu.ar>
2020-10-04 11:40:51 -0300

    Driver lector magnetico

--- ProjectTemplate/drivers/HAL/magnetic_reader/magnetic_reader.h
+++ ProjectTemplate/drivers/HAL/magnetic_reader/magnetic_reader.h
@@ -36,9 +36,13 @@
  */
 void magneticReaderInit(void);
 
- /**
- * @brief Starts reading and saving the card information
- */
+/**
+* @brief Callbacks to the card information and error.
+* @param dataCb callback to get new card event.
+* @param errorCb callback to check for error.
+*/
+void magneticReaderSubscribe(void (*dataCb) (uint8_t data[]), void (*errorCb) (void));
+
 
 
 /*******************************************************************************
