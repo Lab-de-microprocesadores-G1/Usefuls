d0cad94901ee1005640b15da8a3d1cb70429621b
Kammann123 <lucaskammann@hotmail.com>
2020-10-11 19:02:33 -0300

    Refactor al repositorio en general. Agregando actualización a templates

--- Project Resources/drivers/HAL/button/button.h
+++ Project Resources/drivers/HAL/button/button.h
@@ -1,17 +1,19 @@
 /***************************************************************************//**
-  @file     ${file_name}
-  @brief    ...
+  @file     button.h
+  @brief    Buttons driver
   @author   G. Davidov, F. Farall, J. Gaytán, L. Kammann, N. Trozzo
  ******************************************************************************/
 
-#ifndef ${include_guard_symbol}
-#define ${include_guard_symbol}
+#ifndef BUTTON_BUTTON_H_
+#define BUTTON_BUTTON_H_
 
 /*******************************************************************************
  * INCLUDE HEADER FILES
  ******************************************************************************/
 
-${includes}
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 /*******************************************************************************
  * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
@@ -21,17 +23,41 @@ ${includes}
  * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
  ******************************************************************************/
 
-/*******************************************************************************
- * VARIABLE PROTOTYPES WITH GLOBAL SCOPE
- ******************************************************************************/
+typedef enum {
+	BUTTON_RELEASE,		// Button has been released
+	BUTTON_PRESS,		// Button has been pressed
+	BUTTON_LKP,			// Button has been pressed a long time
+	BUTTON_TYPEMATIC,	// If the button is kept pressed some time, this event is emitted periodically
+	BUTTON_EV_COUNT
+} button_events_t;
+
+// Declare the ID of buttons
+typedef enum {
+	BUTTON_1,
+	BUTTON_2,
+	BUTTON_COUNT
+} button_id_t;
 
 /*******************************************************************************
  * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
  ******************************************************************************/
 
-/*******************************************************************************
- ******************************************************************************/
+/**
+ * @brief Initialize button and corresponding peripheral
+ */
+void buttonInit(void);
 
-${declarations}
+// Non-Blocking services //
 
-#endif /* ${include_guard_symbol} */
+/**
+ * @brief Suscribe to button event
+ */
+void buttonSubscribe(button_id_t id, button_events_t event, void (*callback) (void));
+
+/**
+ * @brief Check if button is pressed
+ * @param id ID of the button
+ */
+bool isButtonPressed(button_id_t id);
+
+#endif /* BUTTON_BUTTON_H_ */
---
+++ Project Resources/drivers/HAL/led/led.c
@@ -0,0 +1,295 @@
+/*******************************************************************************
+  @file     led.c
+  @brief    LED Driver
+  @author   G. Davidov, F. Farall, J. Gaytán, L. Kammann, N. Trozzo
+ ******************************************************************************/
+
+/*******************************************************************************
+ * INCLUDE HEADER FILES
+ ******************************************************************************/
+
+#include "led.h"
+
+#include "drivers/MCAL/gpio/gpio.h"
+#include "drivers/MCAL/systick/systick.h"
+
+#include "board/board.h"
+
+/*******************************************************************************
+ * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
+ ******************************************************************************/
+
+#define SYSTICK_MS		(1 / SYSTICK_ISR_FREQUENCY_HZ * 1000)
+#define M2TICKS(ms)		( (ms) / SYSTICK_MS )
+
+/*******************************************************************************
+ * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
+ ******************************************************************************/
+
+typedef struct {
+	ttick_t		period;		// Period of the blinking mode
+	ttick_t		cnt;		// Counter of the period
+} led_blink_t;
+
+typedef struct {
+	ttick_t		burstPeriod;		// Period of the burst mode
+	ttick_t		blinkPeriod;		// Period of the blink mode
+	uint8_t		amount;				// Amount of blinks in the burst period
+	ttick_t		burstCounter;		// Counter of the period of the burst
+	ttick_t		blinkCounter;		// Counter of the period of each blink
+	uint8_t		amountCounter;		// Counter of number of blinks done
+} led_burst_t;
+
+typedef union {
+	led_blink_t		blink;	// The Blinking mode data in the instance
+	led_burst_t		burst;	// The Burst mode data in the instance
+} led_mode_data_t;
+
+typedef struct {
+	pin_t			pin;			// The pin assigned to the LED
+	bool			enabled;		// Disable used to stop interruptions during change of setting
+	bool			activeState;	// Logic active state of the LED
+	led_mode_t 		currentMode;	// Current mode of operation
+	led_mode_data_t settings;		// Settings of the current mode of operation
+} led_t;
+
+/*******************************************************************************
+ * VARIABLES WITH GLOBAL SCOPE
+ ******************************************************************************/
+
+
+
+/*******************************************************************************
+ * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
+ ******************************************************************************/
+
+static void ledIsr(void);
+static void ledDisable(led_id_t id);
+
+/*******************************************************************************
+ * ROM CONST VARIABLES WITH FILE LEVEL SCOPE
+ ******************************************************************************/
+
+
+
+/*******************************************************************************
+ * STATIC VARIABLES AND CONST VARIABLES WITH FILE LEVEL SCOPE
+ ******************************************************************************/
+
+static led_t leds[AMOUNT_OF_LEDS] = {
+		{PIN_LED_RED, LED_ACTIVE},
+		{PIN_LED_BLUE, LED_ACTIVE}
+};
+
+/*******************************************************************************
+ *******************************************************************************
+                        GLOBAL FUNCTION DEFINITIONS
+ *******************************************************************************
+ ******************************************************************************/
+
+void ledInit(void)
+{
+	// Initializing periodic ISR...
+	systickIsr(ledIsr);
+
+	// Initializing leds...
+	uint8_t index;
+	for(index = 0 ; index < AMOUNT_OF_LEDS ; index++)
+	{
+		// Setting initial state of each LED
+		leds[index].currentMode = STATIC;
+
+		// Configuring the pin and port used by the LED
+		gpioWrite(leds[index].pin, !leds[index].activeState);
+		gpioMode(leds[index].pin, OUTPUT);
+	}
+}
+
+void ledToggle(led_id_t id)
+{
+#ifdef LED_DEVELOPMENT_MODE
+	if (id >= 0 && id < AMOUNT_OF_LEDS)
+#endif
+	{
+		led_t *led = &leds[id];
+		if (led->currentMode == STATIC)
+		{
+			gpioToggle(led->pin);
+		}
+	}
+#ifdef LED_DEVELOPMENT_MODE
+	else
+	{
+		// Raise some error!
+	}
+#endif
+}
+
+void ledTurn(led_id_t id, led_status_t status)
+{
+#ifdef LED_DEVELOPMENT_MODE
+	if (id >= 0 && id < AMOUNT_OF_LEDS)
+#endif
+	{
+		// Disable current configurations of the led
+		ledDisable(id);
+
+		// Change to STATIC mode of operation
+		led_t *led = &leds[id];
+		led->enabled = false;
+		led->currentMode = STATIC;
+		gpioWrite(led->pin, status == ON ? led->activeState : !led->activeState);
+		led->enabled = true;
+	}
+#ifdef LED_DEVELOPMENT_MODE
+	else
+	{
+		// Raise some error!
+	}
+#endif
+}
+
+void ledBlink(led_id_t id, ttick_t period)
+{
+#ifdef LED_DEVELOPMENT_MODE
+	if (id >= 0 && id < AMOUNT_OF_LEDS)
+#endif
+	{
+		// Disable current configurations of the led
+		ledDisable(id);
+
+		// Change to BLINK mode of operation
+		led_t *led = &leds[id];
+		led->enabled = false;
+		led->currentMode = BLINK;
+		led->settings.blink.period = period;
+		led->settings.blink.cnt = period / 2;
+		led->enabled = true;
+	}
+#ifdef LED_DEVELOPMENT_MODE
+	else
+	{
+		// Raise some error!
+	}
+#endif
+}
+
+void ledBurst(led_id_t id, ttick_t burstPeriod, ttick_t blinkPeriod, uint8_t burst)
+{
+#ifdef LED_DEVELOPMENT_MODE
+	if (id >= 0 && id < AMOUNT_OF_LEDS)
+#endif
+	{
+		// Disable current configurations of the led
+		ledDisable(id);
+
+		// Change to BURST mode of operation
+		led_t *led = &leds[id];
+		led->enabled = false;
+		led->currentMode = BURST;
+
+		led->settings.burst.burstPeriod = burstPeriod;
+		led->settings.burst.blinkPeriod = blinkPeriod;
+		led->settings.burst.amount = burst;
+
+		led->settings.burst.burstCounter = burstPeriod / 2;
+		led->settings.burst.blinkCounter = blinkPeriod / 2;
+		led->settings.burst.amountCounter = burst * 2;
+
+		gpioWrite(led->pin, led->activeState);
+		led->enabled = true;
+	}
+#ifdef LED_DEVELOPMENT_MODE
+	else
+	{
+		// Raise some error!
+	}
+#endif
+}
+
+/*******************************************************************************
+ *******************************************************************************
+                        LOCAL FUNCTION DEFINITIONS
+ *******************************************************************************
+ ******************************************************************************/
+
+static void ledDisable(led_id_t id)
+{
+
+#ifdef LED_DEVELOPMENT_MODE
+	if (id >= 0 && id < AMOUNT_OF_LEDS)
+#endif
+	{
+		led_t *led = &leds[id];
+
+		led->enabled = false;
+
+		led->currentMode = STATIC;
+		gpioWrite(led->pin, !led->activeState);
+
+		led->enabled = true;
+	}
+#ifdef LED_DEVELOPMENT_MODE
+	else
+	{
+		// Raise some error!
+	}
+#endif
+}
+
+static void ledIsr(void)
+{
+	uint8_t index;
+	for (index = 0 ; index < AMOUNT_OF_LEDS ; index++)
+	{
+		led_t *led = &leds[index];
+		if (led->enabled)
+		{
+			switch (led->currentMode)
+			{
+				case STATIC:
+					// This mode is not handled by the periodic ISR
+					break;
+
+				case BLINK:
+					led->settings.blink.cnt--;
+					if (led->settings.blink.cnt == 0)
+					{
+						led->settings.blink.cnt = led->settings.blink.period / 2;
+						gpioToggle(led->pin);
+					}
+					break;
+
+				case BURST:
+					led->settings.burst.burstCounter--;
+					if (led->settings.burst.burstCounter == 0)
+					{
+						led->settings.burst.burstCounter = led->settings.burst.burstPeriod / 2;
+						led->settings.burst.blinkCounter = led->settings.burst.blinkPeriod / 2;
+						led->settings.burst.amountCounter = led->settings.burst.amount * 2;
+					}
+					else
+					{
+						if (led->settings.burst.amountCounter > 0)
+						{
+							led->settings.burst.blinkCounter--;
+							if (led->settings.burst.blinkCounter == 0)
+							{
+								led->settings.burst.blinkCounter = led->settings.burst.blinkPeriod / 2;
+								led->settings.burst.amountCounter--;
+								gpioToggle(led->pin);
+							}
+						}
+
+						if (led->settings.burst.amountCounter == 0)
+						{
+							gpioWrite(led->pin, !led->activeState);
+						}
+					}
+					break;
+			}
+		}
+	}
+}
+
+/******************************************************************************/
