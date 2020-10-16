136b4b6eba18738179995d03e0d4130347e5a43e
nicotrozzo <rtrozzo@itba.edu.ar>
2020-10-16 00:34:57 -0300

    ftm header ready

--- Testbenches/ftm_testbench/drivers/MCAL/ftm/ftm.h
+++ Testbenches/ftm_testbench/drivers/MCAL/ftm/ftm.h
@@ -1,6 +1,6 @@
 /***************************************************************************//**
   @file     ftm.h
-  @brief    [...]
+  @brief    FTM Driver
   @author   G. Davidov, F. Farall, J. Gaytán, L. Kammann, N. Trozzo
  ******************************************************************************/
 
@@ -12,67 +12,62 @@
  ******************************************************************************/
 
 #include <stdint.h>
-#include <stdbool.h>
 
 /*******************************************************************************
  * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
  ******************************************************************************/
 
-#define FTM_CH_0 0
-#define FTM_CH_1 1
-#define FTM_CH_2 2
-#define FTM_CH_3 3
-#define FTM_CH_4 4
-#define FTM_CH_5 5
-#define FTM_CH_6 6
-#define FTM_CH_7 7
-
 /*******************************************************************************
  * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
  ******************************************************************************/
 
-typedef enum {FTM_0, FTM_1, FTM_2, FTM_3} FTM_t;
-typedef uint16_t FTMData_t;
-typedef uint32_t FTMChannel_t; /* FTM0/FTM3: Channel 1-8; FTM1/FTM2: Channel 1-2 */
+// FTM Channels
+enum {
+	FTM_CHANNEL_0,
+	FTM_CHANNEL_1,
+	FTM_CHANNEL_2,
+	FTM_CHANNEL_3,
+	FTM_CHANNEL_4,
+	FTM_CHANNEL_5,
+	FTM_CHANNEL_6,
+	FTM_CHANNEL_7,
+	FTM_CHANNEL_COUNT
+};
 
-typedef enum
-{
-	FTMModeInputCapture,
-	FTMModeOutputCompare,
-	FTMModePulseWidthModulation,
-} FTMMode_t;
+// FTM Instances
+enum {
+	FTM_INSTANCE_0,
+	FTM_INSTANCE_1,
+	FTM_INSTANCE_2,
+	FTM_INSTANCE_3,
+	FTM_INSTANCE_COUNT
+};
 
-typedef enum
-{
-	FTMEdgeRising 		= 0x01,
-	FTMEdgeFalling 		= 0x02,
-	FTMEdgeEither 		= 0x03,
-} FTMEdge_t;
+// Input Capture Modes
+typedef enum {
+	FTM_IC_RISING_EDGE,
+	FTM_IC_FALLING_EDGE,
+	FTM_IC_BOTH_EDGES
+} ftm_ic_mode_t;
 
-typedef enum
-{
-	FTMEffectToggle 		= 0x01,
-	FTMEffectClear 			= 0x02,
-	FTMEffectSet 			= 0x03,
-} FTMEffect_t;
+// Output Compare Modes
+typedef enum {
+	FTM_OC_TOGGLE_MATCH,
+	FTM_OC_CLEAR_MATCH,
+	FTM_OC_SET_MATCH
+} ftm_oc_mode_t;
 
-typedef enum
-{
-	FTMLogicAssertedHigh	= 0x02,
-	FTMLocigAssertedLow 	= 0x03,
-} FTMLogic_t;
+// PWM Modes
+typedef enum {
+	FTM_PWM_HIGH_PULSES,
+	FTM_PWM_LOW_PULSES
+} ftm_pwm_mode_t;
 
-typedef enum
-{
-	FTM_PSC_x1		= 0x00,
-	FTM_PSC_x2		= 0x01,
-	FTM_PSC_x4		= 0x02,
-	FTM_PSC_x8		= 0x03,
-	FTM_PSC_x16		= 0x04,
-	FTM_PSC_x32		= 0x05,
-	FTM_PSC_x64		= 0x06,
-	FTM_PSC_x128	= 0x07,
-} FTM_Prescal_t;
+// PWM Alignments
+typedef enum {
+	FTM_PWM_EDGE_ALIGNED,
+	FTM_PWM_CENTER_ALIGNED
+} ftm_pwm_alignment_t;
 
 /*******************************************************************************
  * VARIABLE PROTOTYPES WITH GLOBAL SCOPE
@@ -82,41 +77,104 @@ typedef enum
  * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
  ******************************************************************************/
 
-/**
- * @brief Initialises FTM 
+/*
+ * @brief Initializes FTM
+ * @param instance 		FTM Instance
+ * @param prescaler		Frequency divider
  */
-void        FTMInit                           (void);
-void        FTMInitAsOverflow                 (void);
-void        FTMInitAsOutputCompare            (void);
-void        FTMInitAsInputCapture             (void);
-void        FTMInitAsPWM                      (void);
+void ftmInit(uint8_t instance, uint8_t prescaler);
 
-void        FTM_SetPrescaler                  (FTM_t, FTM_Prescal_t);
-void        FTM_SetModulus                    (FTM_t, FTMData_t);
-FTMData_t   FTM_GetModulus                    (FTM_t);
+/*
+ * @brief Starts FTM
+ * @param instance		FTM Instance
+ */
+void ftmStart(uint8_t instance);
 
-void        FTM_StartClock                    (FTM_t);
-void        FTM_StopClock                     (FTM_t);
+/*
+ * @brief Stops FTM
+ * @param instance		FTM Instance
+ */
+void ftmStop(uint8_t instance);
 
-void        FTM_SetOverflowMode               (FTM_t, bool);
-bool        FTM_IsOverflowPending             (FTM_t);
-void        FTM_ClearOverflowFlag             (FTM_t);
+/*
+ * @brief Returns current value of FTM Channel count
+ */
+uint16_t ftmGetChannelCount(uint8_t instance, uint8_t channel);
 
-void        FTM_SetWorkingMode                (FTM_t, FTMChannel_t, FTMMode_t);
-FTMMode_t   FTM_GetWorkingMode                (FTM_t, FTMChannel_t);
-void        FTM_SetInputCaptureEdge           (FTM_t, FTMChannel_t, FTMEdge_t);
-FTMEdge_t   FTM_GetInputCaptureEdge           (FTM_t, FTMChannel_t);
-void        FTM_SetOutputCompareEffect        (FTM_t, FTMChannel_t, FTMEffect_t);
-FTMEffect_t FTM_GetOutputCompareEffect        (FTM_t, FTMChannel_t);
-void        FTM_SetPulseWidthModulationLogic  (FTM_t, FTMChannel_t, FTMLogic_t);
-FTMLogic_t  FTM_GetPulseWidthModulationLogic  (FTM_t, FTMChannel_t);
+/*
+ * @brief Configures FTM Input Capture Mode
+ * @param instance		FTM Instance
+ * @param channel		FTM Channel
+ * @param mode			FTM Input Capture Mode
+ */
+void ftmInputCaptureInit(uint8_t instance, uint8_t channel, ftm_ic_mode_t mode);
 
-void        FTM_SetCounter 					          (FTM_t, FTMChannel_t, FTMData_t);
-FTMData_t   FTM_GetCounter 					          (FTM_t, FTMChannel_t);
+/*
+ * @brief Configures FTM Output Compare Mode
+ * @param instance		FTM Instance
+ * @param channel		FTM Channel
+ * @param mode			FTM Output Compare Mode
+ * @param count			Number of FTM ticks to count for event
+ */
+void ftmOutputCompareInit(uint8_t instance, uint8_t channel, ftm_oc_mode_t mode, uint16_t count);
 
-void 	      FTM_SetInterruptMode   			      (FTM_t, FTMChannel_t, bool);
-bool 	      FTM_IsInterruptPending 			      (FTM_t, FTMChannel_t);
-void 	      FTM_ClearInterruptFlag 			      (FTM_t, FTMChannel_t);
+/**
+ * @brief Sets FTM output value
+ * @param instance		FTM Instance
+ * @param channel		FTM Channel
+ * @param value			Value to be written
+ */
+void ftmSetOutputValue(uint8_t instance, uint8_t channel, bool value);
+
+/*
+ * @brief Sets FTM count
+ * @param instance		FTM Instance
+ * @param channel		FTM Channel
+ * @param count			Number of FTM ticks to count for next event
+ */
+void ftmSetChannelCount(uint8_t instance, uint8_t channel, uint16_t count);
+
+/*
+ * @brief Configures FTM PWM mode
+ * @param instance		FTM Instance
+ * @param channel		FTM Channel
+ * @param mode			PWM mode
+ * @param alignment		PWM Alignment
+ * @param duty			Duty cycle ticks count
+ * @param period		Period ticks count
+ */
+void ftmPwmInit(uint8_t instance, uint8_t channel, ftm_pwm_mode_t mode, ftm_pwm_alignment_t alignment, uint16_t duty, uint16_t period);
+
+/*
+ * @brief Sets PWM Duty Cycle
+ * @param instance		FTM Instance
+ * @param channel		FTM Channel
+ * @param duty			Duty cycle ticks count
+ */
+void ftmPwmSetDuty(uint8_t instance, uint8_t channel, uint16_t duty);
+
+/*
+ * @brief Registers action to be done on instance overflow
+ * @param instance		FTM Instance
+ * @param callback		Callback to be called on overflow event
+ */
+void ftmOverflowSubscribe(uint8_t instance, void (*callback)(void));
+
+/*
+ * @brief Registers action to be done on channel event
+ * @param instance		FTM Instance
+ * @param channel		FTM Channel
+ * @param callback		Callback to be called on channel event
+ */
+void ftmChannelSubscribe(uint8_t instance, uint8_t channel, void (*callback)(uint16_t));
+
+/*
+ * @brief Configures FTM overflow value
+ * @param instance		FTM Instance
+ * @param modulo		Value to reset the count
+ */
+void ftmOverflowInit(uint8_t instance, uint16_t modulo);
+
 
 /*******************************************************************************
  ******************************************************************************/
