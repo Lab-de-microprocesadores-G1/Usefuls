f202a723eeb528448ab59dcbf2189a89e6224181
Kammann123 <lucaskammann@hotmail.com>
2020-10-17 00:05:36 -0300

    Publicando versión funcionando y actualizado de FlexTimer Module, driver, en Usefuls/Resources

--- Resources/drivers/MCAL/ftm/ftm.c
+++ Resources/drivers/MCAL/ftm/ftm.c
@@ -17,6 +17,8 @@
  * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
  ******************************************************************************/
 
+#define CHANNEL_MASK(x)		(0x00000001 << (x))
+
 /*******************************************************************************
  * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
  ******************************************************************************/
@@ -42,14 +44,14 @@ enum {
  ******************************************************************************/
 
 // FTM instances interruption service routines
-void 	FTM_IRQDispatch(uint8_t instance);
+void 	FTM_IRQDispatch(ftm_instance_t instance);
 __ISR__ FTM0_IRQHandler(void);
 __ISR__ FTM1_IRQHandler(void);
 __ISR__ FTM2_IRQHandler(void);
 __ISR__ FTM3_IRQHandler(void);
 
 // Configure PORT MUX 
-void setFtmChannelMux(uint8_t instance, uint8_t channel);
+void setFtmChannelMux(ftm_instance_t instance, ftm_channel_t channel);
 
 /*******************************************************************************
  * ROM CONST VARIABLES WITH FILE LEVEL SCOPE
@@ -68,7 +70,7 @@ static const uint8_t 	ftmIrqs[] = FTM_IRQS;
 // The one and only one look up table by Nico "El Rafa" Trozzo
 static const pin_t		ftmChannelPins[FTM_INSTANCE_COUNT][FTM_CHANNEL_COUNT] = {
 	//	Channel 0			Channel 1			Channel 2			Channel 3		Channel 4			Channel 5			Channel 6			Channel 7
-	{ PORTNUM2PIN(PC,1),  PORTNUM2PIN(PA,4),  PORTNUM2PIN(PA,5), PORTNUM2PIN(PC,4), PORTNUM2PIN(PD,4), PORTNUM2PIN(PD,5), PORTNUM2PIN(PD,6)	, PORTNUM2PIN(PD,7)  },	// FTM0
+	{ PORTNUM2PIN(PC,1),  PORTNUM2PIN(PA,4),  PORTNUM2PIN(PC,3), PORTNUM2PIN(PC,4), PORTNUM2PIN(PD,4), PORTNUM2PIN(PD,5), PORTNUM2PIN(PD,6)	, PORTNUM2PIN(PD,7)  },	// FTM0
 	{ PORTNUM2PIN(PA,12), PORTNUM2PIN(PA,13), 0				   , 0				  , 0				 , 0			   	, 0				   	, 0				     }, // FTM1
 	{ PORTNUM2PIN(PB,18), PORTNUM2PIN(PB,19), 0				   , 0				  , 0				 , 0			   	, 0				   	, 0				     }, // FTM2
 	{ PORTNUM2PIN(PD,0),  PORTNUM2PIN(PD,1),  PORTNUM2PIN(PD,2), PORTNUM2PIN(PD,3), PORTNUM2PIN(PC,8), PORTNUM2PIN(PC,9), PORTNUM2PIN(PC,10), PORTNUM2PIN(PC,11) }  // FTM3
@@ -93,7 +95,7 @@ static const uint8_t	ftmChannelAlts[FTM_INSTANCE_COUNT][FTM_CHANNEL_COUNT] = {
  *******************************************************************************
  ******************************************************************************/
 
-void ftmInit(uint8_t instance, uint8_t prescaler, uint16_t module)
+void ftmInit(ftm_instance_t instance, uint8_t prescaler, uint16_t module)
 {
 	// Clock gating enable
 	switch(instance)
@@ -135,17 +137,22 @@ void ftmInit(uint8_t instance, uint8_t prescaler, uint16_t module)
 	ftmInstances[instance]->MODE |= FTM_MODE_FTMEN(1);
 }
 
-void ftmStart(uint8_t instance)
+void ftmStart(ftm_instance_t instance)
 {
 	ftmInstances[instance]->SC |= FTM_SC_CLKS(FTM_CLOCK_SYSTEM);
 }
 
-void ftmStop(uint8_t instance)
+void ftmStop(ftm_instance_t instance)
 {
 	ftmInstances[instance]->SC &= (~FTM_SC_CLKS_MASK);
 }
 
-void ftmOverflowSubscribe(uint8_t instance, void (*callback)(void))
+uint16_t ftmGetCount(ftm_instance_t instance)
+{
+	return ftmInstances[instance]->CNT;
+}
+
+void ftmOverflowSubscribe(ftm_instance_t instance, void (*callback)(void))
 {
 	if (callback)
 	{
@@ -157,27 +164,17 @@ void ftmOverflowSubscribe(uint8_t instance, void (*callback)(void))
 	}
 }
 
-void ftmSetOutputValue(uint8_t instance, uint8_t channel, bool value)
-{
-	// Configures the initial value for the output channel
-	uint32_t outputMask = 0x01 << channel;
-	ftmInstances[instance]->OUTINIT = value ? (ftmInstances[instance]->OUTINIT | outputMask) : (ftmInstances[instance]->OUTINIT & (~outputMask));
-
-	// Updates the output state of the selected channel
-	ftmInstances[instance]->MODE |= FTM_MODE_INIT(1);
-}
-
-void ftmSetChannelCount(uint8_t instance, uint8_t channel, uint16_t count)
+void ftmChannelSetCount(ftm_instance_t instance, ftm_channel_t channel, uint16_t count)
 {
 	ftmInstances[instance]->CONTROLS[channel].CnV = count;
 }
 
-uint16_t ftmGetChannelCount(uint8_t instance, uint8_t channel)
+uint16_t ftmChannelGetCount(ftm_instance_t instance, ftm_channel_t channel)
 {
 	return ftmInstances[instance]->CONTROLS[channel].CnV;
 }
 
-void ftmChannelSubscribe(uint8_t instance, uint8_t channel, void (*callback)(uint16_t))
+void ftmChannelSubscribe(ftm_instance_t instance, ftm_channel_t channel, void (*callback)(uint16_t))
 {
 	if (callback)
 	{
@@ -189,7 +186,7 @@ void ftmChannelSubscribe(uint8_t instance, uint8_t channel, void (*callback)(uin
 	}
 }
 
-void ftmInputCaptureInit(uint8_t instance, uint8_t channel, ftm_ic_mode_t mode)
+void ftmInputCaptureInit(ftm_instance_t instance, ftm_channel_t channel, ftm_ic_mode_t mode)
 {
 	// Channel set to input capture on given edge/s
 	ftmInstances[instance]->CONTROLS[channel].CnSC = FTM_CnSC_ELSA(mode == FTM_IC_RISING_EDGE ? 0 : 1) | FTM_CnSC_ELSB(mode == FTM_IC_FALLING_EDGE ? 0 : 1);
@@ -198,20 +195,38 @@ void ftmInputCaptureInit(uint8_t instance, uint8_t channel, ftm_ic_mode_t mode)
 	setFtmChannelMux(instance, channel);
 }
 
-void ftmOutputCompareInit(uint8_t instance, uint8_t channel, ftm_oc_mode_t mode, uint16_t count)
+void ftmOutputCompareInit(ftm_instance_t instance, ftm_channel_t channel, ftm_oc_mode_t mode, bool outInit)
 {
 	// Configuration of the channel as output compare
 	ftmInstances[instance]->CONTROLS[channel].CnSC = FTM_CnSC_MSB(0) | FTM_CnSC_MSA(1) | FTM_CnSC_ELSB(mode == FTM_OC_TOGGLE ? 0 : 1) | FTM_CnSC_ELSA(mode == FTM_OC_CLEAR ? 0 : 1);
-	ftmInstances[instance]->CONTROLS[channel].CnV = ftmInstances[instance]->CNT + count;
 
 	// Pin MUX alternative
 	setFtmChannelMux(instance, channel);
 
-	// Enables the matching process on the selected channel
-	ftmInstances[instance]->PWMLOAD |= (0x00000001 << channel);
+	// Sets the initial value of the output channel
+	uint32_t outputMask = CHANNEL_MASK(channel);
+	ftmInstances[instance]->OUTINIT = outInit ? (ftmInstances[instance]->OUTINIT | outputMask) : (ftmInstances[instance]->OUTINIT & (~outputMask));
 }
 
-void ftmPwmInit(uint8_t instance, uint8_t channel, ftm_pwm_mode_t mode, ftm_pwm_alignment_t alignment, uint16_t duty, uint16_t period)
+void ftmOutputCompareStart(ftm_instance_t instance, ftm_channel_t channel, uint16_t count)
+{
+	// Forces the output channel to its initial value registered during the initialization process
+	ftmInstances[instance]->MODE |= FTM_MODE_INIT(1);
+
+	// Enables the matching process on the selected channel and updates the current count
+	ftmInstances[instance]->CONTROLS[channel].CnV = ftmInstances[instance]->CNT + count;
+	ftmInstances[instance]->PWMLOAD |= CHANNEL_MASK(channel);
+	ftmInstances[instance]->OUTMASK &= (~CHANNEL_MASK(channel));
+}
+
+void ftmOutputCompareStop(ftm_instance_t instance, ftm_channel_t channel)
+{
+	// Disables the matching process on the PWMLOAD register
+	ftmInstances[instance]->PWMLOAD &= (~CHANNEL_MASK(channel));
+	ftmInstances[instance]->OUTMASK |= CHANNEL_MASK(channel);
+}
+
+void ftmPwmInit(ftm_instance_t instance, ftm_channel_t channel, ftm_pwm_mode_t mode, ftm_pwm_alignment_t alignment, uint16_t duty, uint16_t period)
 {
 	// Configure up or up/down counter 
 	ftmInstances[instance]->SC |= FTM_SC_CPWMS(alignment == FTM_PWM_CENTER_ALIGNED ? 1 : 0);
@@ -220,7 +235,7 @@ void ftmPwmInit(uint8_t instance, uint8_t channel, ftm_pwm_mode_t mode, ftm_pwm_
 	ftmInstances[instance]->CONTROLS[channel].CnSC = FTM_CnSC_MSB(1) | FTM_CnSC_ELSB(1) | FTM_CnSC_ELSA(mode == FTM_PWM_LOW_PULSES ? 1 : 0);
 	
 	// Enable changes on MOD, CNTIN and CnV
-	ftmInstances[instance]->PWMLOAD |= FTM_PWMLOAD_LDOK(1) | (0x00000001 << channel);
+	ftmInstances[instance]->PWMLOAD |= FTM_PWMLOAD_LDOK(1) | CHANNEL_MASK(channel);
 
 	// Configure PWM period and duty
 	ftmInstances[instance]->CNTIN = 0;
@@ -229,11 +244,40 @@ void ftmPwmInit(uint8_t instance, uint8_t channel, ftm_pwm_mode_t mode, ftm_pwm_
 	
 	// Pin MUX alternative
 	setFtmChannelMux(instance, channel);
+
+	// Enable Synchronization
+	ftmInstances[instance]->COMBINE |= (FTM_COMBINE_SYNCEN0_MASK << (8 * (channel / 2)));
+
+	// Advanced synchronization, and enable Software Trigger to change CnV!
+	ftmInstances[instance]->SYNCONF |= FTM_SYNCONF_SYNCMODE_MASK | FTM_SYNCONF_SWWRBUF_MASK;
+
+	// Sync when CNT == MOD - 1
+	ftmInstances[instance]->SYNC |= FTM_SYNC_CNTMAX_MASK;
 }
 
-void ftmPwmSetDuty(uint8_t instance, uint8_t channel, uint16_t duty)
+void ftmPwmSetDuty(ftm_instance_t instance, ftm_channel_t channel, uint16_t duty)
 {
-	ftmInstances[instance]->CONTROLS[channel].CnV = duty - 1;
+	// Software Trigger
+	ftmInstances[instance]->SYNC |= FTM_SYNC_SWSYNC_MASK;
+
+	// Change Duty
+	ftmInstances[instance]->CONTROLS[channel].CnV = duty;
+}
+
+void ftmPwmSetEnable(ftm_instance_t instance, ftm_channel_t channel, bool running)
+{
+	// Software Trigger
+	ftmInstances[instance]->SYNC |= FTM_SYNC_SWSYNC_MASK;
+
+	// Change OutMask
+	if (running)
+	{
+		ftmInstances[instance]->OUTMASK &= (~CHANNEL_MASK(channel));
+	}
+	else
+	{
+		ftmInstances[instance]->OUTMASK |= CHANNEL_MASK(channel);
+	}
 }
 
 /*******************************************************************************
@@ -241,7 +285,7 @@ void ftmPwmSetDuty(uint8_t instance, uint8_t channel, uint16_t duty)
                         LOCAL FUNCTION DEFINITIONS
  *******************************************************************************
  ******************************************************************************/
-void FTM_IRQDispatch(uint8_t instance)
+void FTM_IRQDispatch(ftm_instance_t instance)
 {
 	// Verify if the interruption occurred because of the overflow
 	// or because of a matching process in any of the timer channels
@@ -259,19 +303,22 @@ void FTM_IRQDispatch(uint8_t instance)
 	}
 	else
 	{
-		for (uint8_t channel = 0; channel < FTM_CHANNEL_COUNT; channel++)
-		{
-			if (ftmInstances[instance]->CONTROLS[channel].CnSC & FTM_CnSC_CHF_MASK)
-			{
-				// Clear the interruption flag
-				ftmInstances[instance]->CONTROLS[channel].CnSC &= (~FTM_CnSC_CHF_MASK);
+		// Save current status of the interruption flags for all channels
+		uint32_t status = ftmInstances[instance]->STATUS;
 
-				// Calls the callback registered (if any)
-				ch_callback channelCallback = ftmChannelCallbacks[instance][channel];
-				if (channelCallback)
-				{
-					channelCallback(ftmInstances[instance]->CONTROLS[channel].CnV);
-				}
+		// Clear flags in the FlexTimer status register
+		ftmInstances[instance]->STATUS = 0x00000000;
+
+		for (ftm_channel_t channel = 0; channel < FTM_CHANNEL_COUNT; channel++)
+		{
+			// Fetch channel interruption callback
+			ch_callback channelCallback = ftmChannelCallbacks[instance][channel];
+
+			// If interruption callback registered, verify if flag was active...
+			// laziness will prevent shifting and masking status when no callback registered
+			if (channelCallback && (status & CHANNEL_MASK(channel)))
+			{
+				channelCallback(ftmInstances[instance]->CONTROLS[channel].CnV);
 			}
 		}
 	}
@@ -297,7 +344,7 @@ __ISR__ FTM3_IRQHandler(void)
 	FTM_IRQDispatch(FTM_INSTANCE_3);
 }
 
-void setFtmChannelMux(uint8_t instance, uint8_t channel)
+void setFtmChannelMux(ftm_instance_t instance, ftm_channel_t channel)
 {
 	PORT_Type* 	ports[] = PORT_BASE_PTRS;
 	pin_t 		pin = ftmChannelPins[instance][channel];
