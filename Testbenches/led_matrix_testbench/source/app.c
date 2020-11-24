3f7ec2550e4b77fff20c9a101a4f7dbd9212f2cf
nicotrozzo <rtrozzo@itba.edu.ar>
2020-11-24 09:43:02 -0300

    Moved pwm_dma to Resources and created led_matrix testbench

--- Testbenches/led_matrix_testbench/source/app.c
+++ Testbenches/led_matrix_testbench/source/app.c
@@ -8,34 +8,32 @@
  * INCLUDE HEADER FILES
  ******************************************************************************/
 
-#include <stdio.h>
-
-#include "board.h"
-#include "drivers/MCAL/pwm_dma/pwm_dma.h"
+// #include "superpower.h"
 
 /*******************************************************************************
  * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
  ******************************************************************************/
 
-#define FRAME_SIZE          24 * 8
-#define TOTAL_FRAMES        8
-#define BUFFER_SIZE         FRAME_SIZE
-
-#define PRESCALER           1
-#define MODULO              30
+// #define SOME_CONSTANT    20
+// #define MACRO(x)         (x)
 
 /*******************************************************************************
  * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
  ******************************************************************************/
 
-static void updateCallback (uint16_t * frameToUpdate, uint8_t frameCounter);
+// static void privateFunction(void);
+
+/*******************************************************************************
+ * VARIABLES TYPES DEFINITIONS
+ ******************************************************************************/
+
+// typedef int  my_int_t;
 
 /*******************************************************************************
  * PRIVATE VARIABLES WITH FILE LEVEL SCOPE
  ******************************************************************************/
 
-static uint16_t     firstFrame[BUFFER_SIZE];
-static uint16_t     secondFrame[BUFFER_SIZE];
+// static int myVar;
 
 /*******************************************************************************
  *******************************************************************************
@@ -46,20 +44,13 @@ static uint16_t     secondFrame[BUFFER_SIZE];
 /* Called once at the beginning of the program */
 void appInit (void)
 {
-    // Initialize PWM-DMA driver
-    pwmdmaInit(PRESCALER, MODULO, FTM_INSTANCE_0, FTM_CHANNEL_0);
-
-    // Register update callback
-    pwmdmaOnFrameUpdate(updateCallback);
-
-    // Start the DMA transfer on the PWM
-    pwmdmaStart(firstFrame, secondFrame, FRAME_SIZE, TOTAL_FRAMES, false);
+    // Application initialisation, drivers, etc...
 }
 
 /* Called repeatedly in an infinite loop */
 void appRun (void)
 {
-
+    // Application iterative tasks, every loop runs this function
 }
 
 /*******************************************************************************
@@ -67,14 +58,6 @@ void appRun (void)
                         LOCAL FUNCTION DEFINITIONS
  *******************************************************************************
  ******************************************************************************/
-
-static void updateCallback (uint16_t * frameToUpdate, uint8_t frameCounter)
-{
-    for (uint8_t i = 0 ; i < FRAME_SIZE ; i++)
-    {
-        frameToUpdate[i] = i % 2 ? 10 : 20;
-    }
-}
 
 
 /*******************************************************************************
--- Testbenches/led_matrix_testbench/startup/main.c
+++ Testbenches/led_matrix_testbench/startup/main.c
@@ -1,41 +1,43 @@
-/********************************************************************************
-  @file     App.c
-  @brief    Application functions
+/*******************************************************************************
+  @file     main.c
+  @brief    Main application
   @author   N. Magliola, G. Davidov, F. Farall, J. Gaytán, L. Kammann, N. Trozzo
- *******************************************************************************/
+ ******************************************************************************/
 
 /*******************************************************************************
  * INCLUDE HEADER FILES
  ******************************************************************************/
 
-#include <stdio.h>
-
-#include "board.h"
-#include "drivers/MCAL/pwm_dma/pwm_dma.h"
+#include "hardware.h"
 
 /*******************************************************************************
  * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
  ******************************************************************************/
 
-#define FRAME_SIZE          24 * 8
-#define TOTAL_FRAMES        8
-#define BUFFER_SIZE         FRAME_SIZE
 
-#define PRESCALER           1
-#define MODULO              30
+/*******************************************************************************
+ * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * VARIABLES WITH GLOBAL SCOPE
+ ******************************************************************************/
 
 /*******************************************************************************
  * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
  ******************************************************************************/
 
-static void updateCallback (uint16_t * frameToUpdate, uint8_t frameCounter);
+void appInit (void);
+void appRun (void);
 
 /*******************************************************************************
- * PRIVATE VARIABLES WITH FILE LEVEL SCOPE
+ * ROM CONST VARIABLES WITH FILE LEVEL SCOPE
  ******************************************************************************/
 
-static uint16_t     firstFrame[BUFFER_SIZE];
-static uint16_t     secondFrame[BUFFER_SIZE];
+/*******************************************************************************
+ * STATIC VARIABLES AND CONST VARIABLES WITH FILE LEVEL SCOPE
+ ******************************************************************************/
 
 /*******************************************************************************
  *******************************************************************************
@@ -43,24 +45,6 @@ static uint16_t     secondFrame[BUFFER_SIZE];
  *******************************************************************************
  ******************************************************************************/
 
-/* Called once at the beginning of the program */
-void appInit (void)
-{
-    // Initialize PWM-DMA driver
-    pwmdmaInit(PRESCALER, MODULO, FTM_INSTANCE_0, FTM_CHANNEL_0);
-
-    // Register update callback
-    pwmdmaOnFrameUpdate(updateCallback);
-
-    // Start the DMA transfer on the PWM
-    pwmdmaStart(firstFrame, secondFrame, FRAME_SIZE, TOTAL_FRAMES, false);
-}
-
-/* Called repeatedly in an infinite loop */
-void appRun (void)
-{
-
-}
 
 /*******************************************************************************
  *******************************************************************************
@@ -68,14 +52,12 @@ void appRun (void)
  *******************************************************************************
  ******************************************************************************/
 
-static void updateCallback (uint16_t * frameToUpdate, uint8_t frameCounter)
+int main (void)
 {
-    for (uint8_t i = 0 ; i < FRAME_SIZE ; i++)
-    {
-        frameToUpdate[i] = i % 2 ? 10 : 20;
-    }
+    hardwareInit();
+    hardwareDisableInterrupts();
+    appInit();       /* Program-specific setup */
+    hardwareEnableInterrupts();
+    __FOREVER__
+        appRun();    /* Program-specific loop  */
 }
-
-
-/*******************************************************************************
- ******************************************************************************/
